1fadb4d0d4615b7380d5d31a741c0dd0f6428c10
cho sangdhyun <sangducks@gmail.com>
2021-11-07 00:24:36 +0900

    TEST

--- ports/cgdk-buffer/portfile.cmake
+++ ports/cgdk-buffer/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO CGLabs/CGDK.buffer
     REF 806c0f27ccff5265e66c67bcbe0704a1428a408d
-    SHA512 0
+    SHA512 cbbe24361165d3fbacdda06cbff09429ea220b9fb95c8af0024ed915dd699780fd0ddfcd2e45de0efffdbd013d447a0a4e468ec986fe289439e1dfe91d3549e8
     HEAD_REF master
 )
 
